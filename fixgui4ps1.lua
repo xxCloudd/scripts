b6b0b27a4fc57b2b3f9972ac866a8c3a3b7c8289
bvthxry <93049912+xxCloudd@users.noreply.github.com>
2022-12-10 03:23:57 +0000

    Create fixgui4ps1.lua

---
+++ fixgui4ps1.lua
@@ -0,0 +1,233 @@
+local Lib = {
+    [77002] = 'Festive Dragon',
+    [9004] = 'Wavy Cheeta',
+    [7004] = 'Dragon',
+    [8004] = 'Demon',
+    [5004] = 'Bat',
+    [6004] = 'Ladybug',
+    [3004] = 'Pig',
+    [4004] = 'Seal',
+    [1004] = 'Lamb',
+    [2004] = 'Mouse',
+    [16012] = 'Cyborg Dominus',
+    [16009] = 'Cyborg Demon',
+    [77003] = 'Snow Spike',
+    [15005] = 'Green Gummy Bear',
+    [13007] = 'Space Dragon',
+    [10004] = 'Electric Slime',
+    [66002] = 'Zombie Dog',
+    [15006] = 'Yellow Gummy Bear',
+    [15008] = 'Rainbow',
+    [16002] = 'Cyborg Dog',
+    [12003] = 'Angel',
+    [13001] = 'Alien',
+    [15009] = 'Dark Candy Corn',
+    [17003] = 'Dominus Noob',
+    [66003] = 'Spider',
+    [12007] = 'Mortuus',
+    [12005] = 'Ice Queen',
+    [11004] = 'Lava Watermelon',
+    [76004] = 'Festive Racoon',
+    [66004] = 'Pumpkin',
+    [16006] = 'Red Space Ranger',
+    [16001] = 'Cyborg Cat',
+    [11002] = 'Sherbert',
+    [76001] = 'Festive Cat',
+    [66001] = 'Zombie Cat',
+    [79003] = 'Giant Penguin',
+    [15007] = 'Cookie',
+    [1001] = 'Cat',
+    [3001] = 'Dalmatian',
+    [2001] = 'Brown Cat',
+    [5001] = 'Owl',
+    [4001] = 'Koala',
+    [7001] = 'Watermelon',
+    [6001] = 'Cheeta',
+    [9001] = 'Fantasy Dragon',
+    [8001] = 'Bomb',
+    [66006] = 'Ghostdeeri',
+    [12004] = 'Fire King',
+    [90003] = 'Red Snake',
+    [78002] = 'Gingerbread',
+    [17006] = 'Dominus Damnee',
+    [10003] = 'Lava Turtle',
+    [15001] = 'Candy Cane',
+    [18002] = 'Spike',
+    [17009] = 'Dominus Huge',
+    [14005] = 'Space Bear',
+    [2005] = 'White Bunny',
+    [1005] = 'Orange Cat',
+    [10002] = 'Lava Dalmatian',
+    [8005] = 'Cyclops',
+    [9005] = 'Wavy Tiger',
+    [4005] = 'Racoon',
+    [3005] = 'Panda',
+    [13004] = 'Space Cat',
+    [5005] = 'Bee',
+    [18006] = 'Agony',
+    [18001] = 'Aesthetic Cat',
+    [13002] = 'Puffer',
+    [10001] = 'Lava Zebra',
+    [17007] = 'Dominus Rainbow',
+    [90001] = 'BIG Maskot',
+    [11005] = 'Wavy Bee',
+    [10006] = 'Dominus Messor',
+    [14004] = 'Space Cyclops',
+    [17001] = 'Dominus Pumpkin',
+    [12002] = 'Reaper',
+    [2002] = 'White Cat',
+    [3002] = 'Bear',
+    [4002] = 'Crocodile',
+    [5002] = 'Monkey',
+    [6002] = 'Tiger',
+    [7002] = 'Yeti',
+    [8002] = 'Ghost',
+    [9002] = 'Cherry Bomb',
+    [11006] = 'Unicorn',
+    [90010] = 'Huge Cat',
+    [15004] = 'Green Lollipop',
+    [16011] = 'C0RE SH0CK',
+    [90008] = 'Noob',
+    [1002] = 'Dog',
+    [12006] = 'Immortuos',
+    [90009] = 'Love Cat',
+    [14009] = '1NE',
+    [76002] = 'Festive Dog',
+    [90006] = 'Dominus Partner',
+    [90005] = 'Wavy Snake',
+    [16004] = 'Cyborg',
+    [90004] = 'Purple Snake',
+    [10008] = 'Dominus Empyreus',
+    [90002] = 'Green Snake',
+    [79002] = 'Festive C0RE',
+    [13003] = 'Wooga',
+    [79001] = 'Festive Immortuos',
+    [78005] = 'Randolph',
+    [78004] = 'Festive Dominus',
+    [12001] = 'Tank',
+    [78003] = 'Reindeer',
+    [78001] = 'Festive Ame Damnee',
+    [18004] = 'Chimera',
+    [77001] = 'Festive Seal',
+    [13005] = 'Space Dog',
+    [76003] = 'Festive Bunny',
+    [90007] = 'Love Dog',
+    [66010] = 'Skeleton Ghost',
+    [66009] = 'Willow Wisp Green',
+    [66008] = 'Willow Wisp',
+    [66007] = 'Sorrow',
+    [66005] = 'Skeleton',
+    [18005] = 'Hydra',
+    [77004] = 'Ice Spike',
+    [18003] = 'Magic Fox',
+    [17008] = 'Dominus Electric',
+    [17005] = 'Dominus HeadStack',
+    [13006] = 'Space Bunny',
+    [16005] = 'Space Ranger',
+    [17002] = 'Dominus Cherry',
+    [16010] = 'C0RE',
+    [16008] = 'RoVer',
+    [16007] = 'Friendly Cyborg',
+    [17004] = 'Dominus Wavy',
+    [14002] = 'HeadStack',
+    [16003] = 'Computer',
+    [15011] = 'Domortuus',
+    [15010] = 'Ame Damnee',
+    [15003] = 'Red Lollipop',
+    [15002] = 'Candy Corn',
+    [10007] = 'Dominus Frigidus',
+    [14008] = 'ZER0',
+    [6003] = 'Zebra',
+    [5003] = 'Turtle',
+    [4003] = 'Platypus',
+    [3003] = 'Polar Bear',
+    [2003] = 'White Dog',
+    [1003] = 'Bunny',
+    [14007] = 'Dark Saturn',
+    [10005] = 'Electric Ghost',
+    [14006] = 'Saturn',
+    [14003] = 'Space Owl',
+    [14001] = 'Revurse',
+    [11003] = 'Robot',
+    [11001] = 'Zombie',
+    [9003] = 'Wavy Zebra',
+    [8003] = 'Cherry Monkey',
+    [7003] = 'Slime'
+}
+
+local l = game.Players.LocalPlayer
+
+getPetType = function(Pet)
+    local prefix = ""
+    if Pet.dm then
+        prefix = prefix .. "[DM] "
+    end
+    if Pet.r then
+        prefix = prefix .. "[RB] "
+    end
+    return prefix
+end
+
+getPets = function(save)
+    local pets = {}
+    for i, v in pairs(save.Pets) do 
+        table.insert(pets, {
+            name = Lib[tonumber(v.n)],
+            type = getPetType(v),
+            id = v.id,
+            equipped = v.e,
+            lvl = v.l
+        })
+    end
+    return pets
+end
+
+gui = game:GetObjects("rbxassetid://11781558353")[1]
+gui.Parent = game.CoreGui
+gui.Frame.Draggable=true
+gui.Frame.Active=true
+
+running = true
+gui.Frame.close.MouseButton1Click:connect(function()
+    running = false
+    gui:destroy()
+end)
+
+for i, v in pairs(gui.Frame.ScrollingFrame:children()) do 
+    if v:isA("Frame") then
+        v.deletepet.MouseButton1Click:Connect(function()
+            workspace.__REMOTES.Game.Inventory:InvokeServer("Delete",v.petId.Value)
+        end)
+
+        v.equippet.MouseButton1Click:Connect(function()
+            workspace.__REMOTES.Game.Inventory:InvokeServer(v.isEquipped.Value == true and "Unequip" or "Equip",v.petId.Value)
+        end)
+    end
+    
+end
+
+while running do
+    wait(.1)
+    local Save = workspace.__REMOTES.Core:FindFirstChild("Get Other Stats"):InvokeServer()[l.Name].Save
+    local mooncoins = Save.MoonCoins
+    local coins = Save.Coins
+    local snowcoins = Save.SnowCoins
+    gui.Frame.Coinslabel.Text="  Coins: "..coins.."\n  MoonCoins: "..mooncoins.."\n  SnowCoins: "..snowcoins
+
+    for i, v in pairs(gui.Frame.ScrollingFrame:children()) do 
+        if v:isA("Frame") then
+            v.petId.Value = 0
+            v.TextLabel.Text = ""
+            v.isEquipped.Value = false
+        end
+    end
+
+    local pets = getPets(Save)
+
+    for petNumber, petData in pairs(pets) do
+        local frame = gui.Frame.ScrollingFrame[tostring(petNumber)]
+        frame.petId.Value = petData.id
+        frame.TextLabel.Text = (petData.equipped and " [EQ]" or " ").. petData.type .. petData.name
+        frame.isEquipped.Value = petData.equipped
+    end
+end
