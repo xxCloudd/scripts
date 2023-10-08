2f3bb039fc932f8ad3baeae2c0c82491ec51c6a0
bvthxry <93049912+xxCloudd@users.noreply.github.com>
2023-10-08 11:48:29 +0100

    Update pslbls.lua

--- pslbls.lua
+++ pslbls.lua
@@ -1,9 +1,9 @@
 function add(frame)
 	if frame:isA'Frame' then
-		if frame:FindFirstChild'TextLabel' then
+		if frame:FindFirstChild'TextButton' then
 			frame.TextLabel.Text=frame.Name
 		else
-			local a=Instance.new("TextLabel",frame)
+			local a=Instance.new("TextButton",frame)
 			a.Text=frame.Name
 			a.BackgroundTransparency = 1
 			a.Size = UDim2.new(1,0,0,20)
@@ -11,12 +11,16 @@ function add(frame)
 			a.TextColor3=Color3.new(0,1,0)
 			a.TextStrokeTransparency=.5
 			a.TextSize=14
+            a.MouseButton1Click:Connect(function()
+                setclipboard(a.Parent.Name)
+            end)
 		end
 	end
 end
 
-while task.wait(1) do
+while true do
 	for i, v in pairs(game.Players.LocalPlayer.PlayerGui.Inventory.Frame.Pets:children()) do
 		add(v)
 	end
+    task.wait(1)
 end
