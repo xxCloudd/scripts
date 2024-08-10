d9fe4360438fc2537a93293c236d7dbbcfa38919
bvthxry <93049912+xxCloudd@users.noreply.github.com>
2024-08-10 17:59:37 +0100

    Update and rename pslbls.lua to pet_sim/labels.lua

--- pet_sim/labels.lua
+++ pet_sim/labels.lua
@@ -1,16 +1,19 @@
 function add(frame)
 	if frame:isA'Frame' then
 		if frame:FindFirstChild'TextButton' then
-			frame.TextButton.Text=frame.Name
+			local N = frame.Name
+			frame.TextButton.Text = N
+			frame.TextButton.Visible = N:match'E'~=nil
 		else
 			local a=Instance.new("TextButton",frame)
+			a.Visible = a.Text:match'E'~=nil
 			a.Text=frame.Name
 			a.BackgroundTransparency = 1
-			a.Size = UDim2.new(1,0,0,20)
-			a.ZIndex=20
-			a.TextColor3=Color3.new(0,1,0)
-			a.TextStrokeTransparency=.5
-			a.TextSize=14
+			a.Size = UDim2.new(1, 0, 0, 20)
+			a.ZIndex = 20
+			a.TextColor3 = Color3.new(0,1,0)
+			a.TextStrokeTransparency = .5
+			a.TextSize = 14
             		a.MouseButton1Click:Connect(function()
                 		setclipboard(a.Parent.Name)
             		end)
