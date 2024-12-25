8824c44deaf1417f21b6a8b1680deea6f21599f2
bvthxry <93049912+xxCloudd@users.noreply.github.com>
2024-12-25 00:56:44 +0000

    Create snake_purchase_setupvalue_method.lua

---
+++ pet_sim/snake_purchase_setupvalue_method.lua
@@ -0,0 +1,3 @@
+b=game.Players.LocalPlayer.PlayerGui.PremiumShop.Frame.Pets.Pet.Pet.Buy
+setupvalue(getconnections(b.MouseButton1Click)[1].Function, 3, 344286613)
+-- then just go on the shop and click to purchase a pet
