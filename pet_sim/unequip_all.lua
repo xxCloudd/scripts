10636fa3334461f6f31b0e6c779022d19967a6e5
bvthxry <93049912+xxCloudd@users.noreply.github.com>
2025-01-04 12:07:49 +0000

    Create unequip_all.lua

---
+++ pet_sim/unequip_all.lua
@@ -0,0 +1,7 @@
+for _,v in pairs(workspace.__THINGS.Core['Get Stats']:InvokeServer().Save.Pets)do
+  if v.e then
+    task.spawn(function()
+        workspace.__REMOTES.Game.Inventory:InvokeServer('Unequip', v.id)
+    end)
+  end
+end
