3b53bf25662107516156b8266385767290c9e747
bvthxry <93049912+xxCloudd@users.noreply.github.com>
2025-01-22 19:55:42 +0000

    Update value.lua

--- pet_sim/value.lua
+++ pet_sim/value.lua
@@ -2,7 +2,7 @@ local list = game:HttpGet''
 
 local T,P,tn = 0,require(game.ReplicatedStorage['1 | Directory']).Pets, tonumber
 function z(_) return _.dm and _.r and'gli'or _.g and'g'or _.r and not _.dm and'rb'or _.dm and not _.r and'dm'or'reg' end
-for b,v in pairs(workspace.__THINGS.__REMOTES['Get Stats']:InvokeServer().Save.Pets)do
+for b,v in pairs(workspace.__REMOTES.Core['Get Stats']:InvokeServer().Save.Pets)do
  if v.l then
   for t,ar in pairs(list:split'\n')do
    if z(v)==ar:split'/'[2] then
