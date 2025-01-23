e231456a73173b028d0a8d2fc43109d0317390de
tris <93049912+1ristan@users.noreply.github.com>
2025-01-23 20:37:09 +0000

    Update Latest_Trade_ID_Tracker.lua

--- pet_sim/Trading/Latest_Trade_ID_Tracker.lua
+++ pet_sim/Trading/Latest_Trade_ID_Tracker.lua
@@ -1,6 +1,7 @@
 local tradeId=0
 local lastupdate=nil
 local h = Instance.new('Hint', workspace)
+h.Text='waiting for trade update'
 game:FindFirstChild('Trade Update', true).OnClientEvent:Connect(function(id)
     lastupdate=id
     h.Text='Latest Trade Update ID: ' .. lastupdate .. ' | ' .. os.clock()
